d48cc957d6b76ccb7e8827bcfdc3955cf5c66cc8
alex-mydatasinc <alexjose.r.r@gmail.com>
2024-05-25 16:38:26 -0500

    mostrando en modal información de las habitaciones

--- src/app/views/page/agente/clasificacion-hoteles/modal/modal.component.ts
+++ src/app/views/page/agente/clasificacion-hoteles/modal/modal.component.ts
@@ -7,9 +7,8 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
-import { HabitacionesService } from 'src/app/core/services/habitaciones.service';
-import * as Toastify from 'toastify-js';
 import { ClasificacionHotelesService } from 'src/app/core/services/clasificacion-hoteles.service';
+import { Habitacion } from 'src/app/core/models/habitaciones.models';
 
 @Component({
   selector: 'app-modal',
@@ -20,12 +19,34 @@ import { ClasificacionHotelesService } from 'src/app/core/services/clasificacion
 })
 export class ModalComponent {
 
+  habitaciones: Habitacion[] = [];
+  currentPage: number = 1;
+  totalPages: number = 0;
+  perPage: number = 10;
+  totalItems: number = 0;
+  pageNumbers: number[] = [];
+
   constructor(
     private ClasificacionHotelesService:  ClasificacionHotelesService,
     public dialogRef: MatDialogRef<ModalComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data: { hotelId: number; habitacionId?: number }
   ) {}
+
+  hotelForm: FormGroup = new FormGroup({
+    search : new FormControl(''),
+  })
+
+
+  hasError(field: string): boolean {
+    const errorsObjetc = this.hotelForm.get(field)?.errors ?? {};
+    const errors = Object.keys(errorsObjetc);
+
+    if(errors.length && (this.hotelForm.get(field)?.touched || this.hotelForm.get(field)?.dirty)){
+      return true;
+    }
+    return false;
+  }
 
   onClose(): void {
     this.dialogRef.close();
@@ -41,6 +62,11 @@ export class ModalComponent {
 
   private loadhabitaciones (habitacionId: number) {
     this.ClasificacionHotelesService.getHabitacions(habitacionId).subscribe(response => {
+      this.habitaciones = response.data;
+      this.currentPage = response.current_page;
+      this.totalPages = response.last_page;
+      this.perPage = response.per_page;
+      this.totalItems = response.total;
       console.log('habitaciones: ', response);
     });
   }
